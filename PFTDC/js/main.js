a50c8249231c130e1378608db9f615a10436fd64
Nurbergen-me <nurik.eleshov@gmail.com>
2022-03-11 09:50:43 +0600

    vacancy page

--- PFTDC/js/main.js
+++ PFTDC/js/main.js
@@ -226,3 +226,28 @@ $(document).ready(function(){
   });
 
 })
+
+// FIle input
+
+var inputs = document.querySelectorAll( '.form__file_input' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+  var label	 = input.nextElementSibling,
+      labelVal = label.innerHTML;
+
+  input.addEventListener( 'change', function( e )
+  {
+    var fileName = '';
+    if( this.files && this.files.length > 1 ) {
+      fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+    }  else {
+      fileName = e.target.value.split( "C:\\fakepath\\" ).pop();
+    }
+    if( fileName ) {
+      label.innerHTML = fileName;
+    } else {
+      label.innerHTML = labelVal;
+    }
+  });
+});
+
